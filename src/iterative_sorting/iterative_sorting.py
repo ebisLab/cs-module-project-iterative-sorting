268e9755254b695fa4fb3dc27d05850b0cab1734
ebisLab <ebaiden@techie.com>
2020-05-18 19:11:48 -0400

    mvp

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -7,25 +7,35 @@ def selection_sort(arr):
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
+        for j in range(cur_index + 1, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
+        arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
+    return arr
+    # TO-DO: swap
+    # Your code here
 
-
-        # TO-DO: swap
-        # Your code here
-
+    while j > 0 and cur_index < arr[j-1]:
+        arr[j] = arr[j-1]
+        j -= 1
+    # override array
+    arr[j] = cur_index
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
-
-
+    for i in range(0, len(arr)-1):
+        for j in range(0, len(arr)-1 - i):
+            # comparing item with the item on the right
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
 
 
 # STRETCH: implement the Count Sort function below
 def count_sort(arr, maximum=-1):
     # Your code here
-
 
     return arr
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,29 @@
+position = -1
+
+
 def linear_search(arr, target):
     # Your code here
 
-
-    return -1   # not found
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+    return -1  # i thought -1 was also false
 
 
 # Write an iterative implementation of Binary Search
+
+
 def binary_search(arr, target):
+    i = 0
+    while i < len(arr):
+        if arr[i] == target:
+            globals()['position'] = i
+            return i
+        i = i + 1
 
-    # Your code here
-
+    # for i in range(len(arr)):
+    #     # Your code here
+    #     if arr[i] == target:
+    #         return i
 
     return -1  # not found
